168849afb7def27c68413cb26a9ef7f494062949
Jakub Wujec <63107459+kuba1302@users.noreply.github.com>
2023-08-16 22:10:27 +0200

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
-from .charades import CharadesSTA
+# from .charades import CharadesSTA
 from .qvhighlights import QVHighlights
 from .tvsum import TVSum
 from .youtube import YouTubeHighlights
 
-__all__ = ['CharadesSTA', 'QVHighlights', 'TVSum', 'YouTubeHighlights']
+__all__ = ['QVHighlights', 'TVSum', 'YouTubeHighlights']
